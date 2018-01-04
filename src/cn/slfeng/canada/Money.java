0118578c79e8068d303329c1180db29e1fbc8992
BuildTools <unconfigured@null.spigotmc.org>
2018-01-04 17:50:33 +0800

    上传了有Bug的代码请大佬们指教

---
+++ src/cn/slfeng/canada/Money.java
@@ -0,0 +1,65 @@
+package cn.slfeng.canada;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.milkbowl.vault.economy.Economy;
+
+public class Money implements CommandExecutor {
+	public final Canada plugin;
+
+	private static Economy econ = null;
+
+	public Money(Canada plugin) {
+		this.plugin = plugin;
+		plugin.getServer().getPluginCommand("ca").setExecutor(this);
+	}
+
+	public static Map<String, Integer> money = new HashMap<>();// 玩家押的钱
+	public static Map<String, String> jieguo = new HashMap<>();// 玩家赌的结果
+	public static Map<String, UUID> uuid = new HashMap<String, UUID>();// 玩家的UUID
+	
+	public boolean CheckAndSave(CommandSender sender, String[] args) {
+		if (Pattern.compile("^[-\\+]?[\\d]*$").matcher(args[0]).matches() && Integer.parseInt(args[0]) > 0) {// 如果子命令1是整数且大于0
+			OfflinePlayer offp = (OfflinePlayer) sender;
+			Player onp = (Player) sender;
+			if (econ.getBalance(offp) >= Double.parseDouble(args[0])) {// 如果输入的钱数小于存款
+				jieguo.put(sender.getName(), args[2]);// 放进数据
+				uuid.put(sender.getName(), onp.getUniqueId());// 放入uuid
+				econ.withdrawPlayer(onp, Double.parseDouble(args[0]));// 拿走钱款
+				sender.sendMessage("您已成功下注，请耐心等待开奖");
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (args[0] == "reload") {
+
+		} else if (args[1] == "押") {
+			if (args[2] == "大" || args[2] == "小" || args[2] == "单" || args[2] == "双" || args[2] == "大双"
+					|| args[2] == "小双" || args[2] == "大单" || args[2] == "小单") {
+				CheckAndSave(sender, args);
+			} else if (Pattern.compile("^[-\\+]?[\\d]*$").matcher(args[2]).matches() && Integer.parseInt(args[2]) >= 0
+					&& Integer.parseInt(args[2]) <= 27) {// 如果是整数且大于等于0小于等于27
+				CheckAndSave(sender, args);
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ src/cn/slfeng/canada/Random.java
@@ -0,0 +1,113 @@
+package cn.slfeng.canada;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import net.milkbowl.vault.economy.Economy;
+
+public class Random {
+	private static Economy econ = null;
+	static Canada plugin;
+	public static int result;// 上把结果
+	public static Map<String, Boolean> property = new HashMap<>();// 结果的属性
+
+	public static int random() { // 产生0-27的随机数
+		int random = ((int) (Math.random() * 28)) - 1;
+		return random;
+	}
+
+	public static void compute() {// 分析结果，大true小false,单true双false,是极大小true不是false
+		if (result > 13) {
+			property.put("daxiao", true);
+			if (result > 21) {
+				property.put("jidaxiao", true);
+			} else {
+				property.put("jidaxiao", false);
+			}
+		} else {
+			property.put("daxiao", false);
+			if (result < 6) {
+				property.put("jidaxiao", true);
+			} else {
+				property.put("jidaxiao", false);
+			}
+		}
+		if (result % 2 == 1) {
+			property.put("danshuang", true);
+		} else {
+			property.put("danshuang", false);
+		}
+	}
+
+	public static String integrate() {
+		boolean daxiao = property.get("daxiao");
+		boolean danshuang = property.get("danshuang");
+		String dx;
+		String ds;
+		if (daxiao == true) {
+			dx = "大";
+		} else {
+			dx = "小";
+		}
+		if (danshuang == true) {
+			ds = "单";
+		} else {
+			ds = "双";
+		}
+		return dx + ds;
+	}
+	
+	public static void Cycle() {// 循环开奖
+		plugin.getLogger().info("循环开奖已启动");
+		new BukkitRunnable(){
+			@Override
+		    public void run(){
+		    	result = random();// 将random()放进result
+				compute();// 执行结果分析
+				String jg = integrate();
+				Bukkit.broadcastMessage("[加拿大]" + "上把结果已出:" + result);
+				if (Money.money == null || Money.money.size() == 0) {
+					for (Map.Entry<String, String> entry : Money.jieguo.entrySet()) {
+						if (entry.getValue() == jg) {// 如果结果等于上把结果
+							if (jg == "大双" || jg == "小双" || jg == "大单" || jg == "小单") {// 如果是组合
+								if (Bukkit.getPlayer(entry.getKey()) != null) {// 如果该玩家在线
+									econ.depositPlayer(Bukkit.getPlayer(entry.getKey()),
+											Money.money.get(entry.getKey()) * 4.2);
+								} else {// 如果不在线
+									econ.depositPlayer(Bukkit.getOfflinePlayer(Money.uuid.get(entry.getKey())),
+											Money.money.get(entry.getKey()) * 4.2);
+								}
+							} else if (jg == "大" || jg == "小" || jg == "单" || jg == "双") {
+								if (Bukkit.getPlayer(entry.getKey()) != null) {// 如果该玩家在线
+									econ.depositPlayer(Bukkit.getPlayer(entry.getKey()),
+											Money.money.get(entry.getKey()) * 2);
+								} else {// 如果不在线
+									econ.depositPlayer(Bukkit.getOfflinePlayer(Money.uuid.get(entry.getKey())),
+											Money.money.get(entry.getKey()) * 2);
+								}
+							} else {// 如果是赌数字
+								if (Bukkit.getPlayer(entry.getKey()) != null) {// 如果该玩家在线
+									econ.depositPlayer(Bukkit.getPlayer(entry.getKey()),
+											Money.money.get(entry.getKey()) * 12);
+								} else {// 如果不在线
+									econ.depositPlayer(Bukkit.getOfflinePlayer(Money.uuid.get(entry.getKey())),
+											Money.money.get(entry.getKey()) * 12);
+								}
+							}
+						} else {
+							if (Bukkit.getPlayer(entry.getKey()) != null) {// 如果该玩家在线
+								Bukkit.getPlayer(entry.getKey())
+										.sendMessage("您上把赌的结果是：" + entry.getValue() + "很遗憾您没有中奖");
+							}
+						}
+					}
+				} else {
+					Bukkit.broadcastMessage("上把无人下注");
+				}
+		    }
+		}.runTaskTimerAsynchronously(plugin, 30*20L, 180*20L);
+	}
+}
